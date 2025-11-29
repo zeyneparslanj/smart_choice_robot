76f44485e8776ba89c975eca3e6c2f2a3379be14
zeyneparslanj <zynp.arsln274@gmail.com>
2025-11-29 03:06:23 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // Sadece bu satır her şeyi çözecek:
+    // KİLİT NOKTA BURASI:
     base: "/smart_choice_robot/", 
     
     server: {
